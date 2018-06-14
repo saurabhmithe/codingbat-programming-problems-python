a20d7fe1f4594173c0332e0d6214176b64c11745
Saurabh Mithe <saurabhmithe@gmail.com>
2018-06-14 12:46:37 -0700

    completed warmup-2 problems

---
+++ Solutions/warmup-2/array_front9.py
@@ -0,0 +1,12 @@
+# Given an array of ints, return True if one of the first 4 elements
+# in the array is a 9. The array length may be less than 4.
+
+# array_front9([1, 2, 9, 3, 4]) → True
+# array_front9([1, 2, 3, 4, 9]) → False
+# array_front9([1, 2, 3, 4, 5]) → False
+
+
+def array_front9(nums):
+    if len(nums) >= 4:
+        return 9 in nums[:4]
+    return 9 in nums
