b20b85f4adcc9aeefa923d4953509d553185ec27
Saurabh Mithe <saurabhmithe@gmail.com>
2018-06-14 12:06:44 -0700

    completed warmup-1 problems

---
+++ Solutions/warmup-1/front_back.py
@@ -0,0 +1,12 @@
+# Given a string, return a new string where the
+# first and last chars have been exchanged.
+
+# front_back('code') → 'eodc'
+# front_back('a') → 'a'
+# front_back('ab') → 'ba'
+
+
+def front_back(str):
+    if len(str) > 1:
+        return str[-1] + str[1:-1] + str[0]
+    return str
